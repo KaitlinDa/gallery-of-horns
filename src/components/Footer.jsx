ee245280c7de013d5d8a4dd0e234588dc2a8b79e
KAITLIN DAVIS <KAITLIN.A.DAVIS1996@GMAIL.COM>
2023-11-29 17:40:56 -0600

    Added button count clicker

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 function Footer(props) {
-    return <h2> &copy Kaitlin Davis; {props.year}</h2>
+    return <h2> &copy; Kaitlin Davis {props.year}</h2>
   }
 
   export default Footer;
--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import {useState} from 'react';
 
 const HornedBeast = (props) => {
+  const [buttonClicks, setButtonClicks] = useState(0)
+  function handleClick() {
+    setButtonClicks(buttonClicks+1)
+  }
   return (
-    <div>
+    <div onClick={handleClick}>
       <h2>{props.title}</h2>
       <img src={props.imageUrl} alt={props.title} title={props.title} />
       <p>{props.description}</p>
+      <p> ::heart:: {buttonClicks}</p>
     </div>
   );
 };
