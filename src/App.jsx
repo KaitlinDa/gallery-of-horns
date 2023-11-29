7d4c269a3c032043a330d01f3807fce8afd3534c
KAITLIN DAVIS <KAITLIN.A.DAVIS1996@GMAIL.COM>
2023-11-28 23:11:53 -0600

    finished with header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
-function App(){
+function App() {
+  return (
+    <>
+    <Header title="React Tracker" />
+    <Main />
+    <Footer year="2023"/>
+    </>
+  );
+}
 
-  return <h1>I Live!!!!</h1>
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  )
+}
+
+function Main() {
+  return <h2> Main here </h2>
+}
+
+function Footer(props) {
+  return <h2> &copy Kaitlin Davis; {props.year}</h2>
 }
 
 export default App;
