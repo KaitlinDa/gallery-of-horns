c3c5360f35a7d6eb8a271802bddde54b784f8fee
KAITLIN DAVIS <KAITLIN.A.DAVIS1996@GMAIL.COM>
2023-11-29 20:54:36 -0600

    Added lighthouse score

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
@@ -18,3 +19,4 @@ function App() {
 
 
 export default App;
+
